b533240783ddcad3e0f8c8479c6980acf4e0aacf
Jessica Castro <jessica.castros@outlook.com>
2021-11-16 18:10:17 -0300

    Inserting better example of ejs

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, './views'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { example: 'boilerplate example'} );
 });
 
 
